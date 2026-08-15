358ac6e552bfc0356c464ca808af746deb6036e4
Sri <i7srinithi05@gmail.com>
2026-08-15 21:33:47 +0530

    Refactor Header component to use React Router Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import { Navbar,Container,Nav } from 'react-bootstrap'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 function Header(props) {
   return (
-    <>
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Shoppy</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Shoppy
+        </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="main-navbar" />
 
         <Navbar.Collapse id="main-navbar">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/product">Products</Nav.Link>
-            <Nav.Link href="/cart">Cart {props.cartcount}</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/product">
+              Products
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/cart">
+              Cart {props.cartcount}
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    </>
-  )
+  );
 }
 
-export default Header
+export default Header;
